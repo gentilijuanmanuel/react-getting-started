4c81b93aacb142b2bf1bbd7a43ebc2887cc996bc
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-03-12 08:21:55 -0300

    Clock app added.

---
+++ clock/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Clock extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>Hello, world!</h2>
+                <h2>It's {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
+    }
+}
+
+//=======================
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
