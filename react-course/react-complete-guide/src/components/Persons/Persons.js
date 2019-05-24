fe9f63a6239a607c700f261d2f0ac90e5937de6d
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-05-24 16:01:32 -0300

    Added Persons component in react-complete-guide project (my way).

--- react-course/react-complete-guide/src/components/Persons/Persons.js
+++ react-course/react-complete-guide/src/components/Persons/Persons.js
@@ -1,13 +1,53 @@
 import React, { useState } from 'react';
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary.js';
+import Person from '../Persons/Person/Person.js';
 
 const persons = (props) => {
     const [ personsState, setPersonsState ] = useState({
-        
-    }); 
+        persons: props.persons        
+    });
 
-    return(
-        <h1>Hello!</h1>
+    const changeNameHandler = (event, personId) => {
+        const modifiedPersonIndex = personsState.persons.findIndex(person => { return person.id === personId; });
+
+        const persons = [
+            ...personsState.persons
+        ];
+
+        const modifiedPerson = persons[modifiedPersonIndex];
+        modifiedPerson.name = event.target.value;
+
+        setPersonsState({
+            persons: persons,
+            showPersons: personsState.showPersons
+        });
+    }
+
+    const deletePersonHandler = (personIndex) => {
+        const currentPersons = [...personsState.persons];
+        currentPersons.splice(personIndex, 1);
+        
+        setPersonsState({
+            persons: currentPersons,
+            showPersons: personsState.showPersons
+        });
+    }
+
+    const personsToDisplay = personsState.persons.map((person, index) => {
+        return(
+          //ErrorBoundary is a high order component
+          <ErrorBoundary key={person.id}>
+            <Person
+              click={() => deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              changed={(event) => changeNameHandler(event, person.id)} />
+          </ErrorBoundary>
+        );
+      }
     );
+
+    return personsToDisplay;
 }
 
 export default persons;
--- react-course/react-complete-guide/src/containers/App.js
+++ react-course/react-complete-guide/src/containers/App.js
@@ -1,93 +1,56 @@
 import React, { useState } from 'react';
-import Person from '../components/Persons/Person/Person.js';
 import classes from './App.css';
-import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary.js';
 import Persons from '../components/Persons/Persons.js';
 
-const app = props => {
+const app = () => {
   const [ appState, setAppState ] = useState({
-    persons: [
-      { id: 'dfeuf', name: "Juanma", age: 23 },
-      { id: 'fdfdf', name: "Octavio", age: 22 },
-      { id: '2e2e', name: "Julián", age: 25 }
-    ],
+    // persons: [
+    //   { id: 'dfeuf', name: "Juanma", age: 23 },
+    //   { id: 'fdfdf', name: "Octavio", age: 22 },
+    //   { id: '2e2e', name: "Julián", age: 25 }
+    // ],
     showPersons: false
   });
 
+  const persons = [
+    { id: 'dfeuf', name: "Juanma", age: 23 },
+    { id: 'fdfdf', name: "Octavio", age: 22 },
+    { id: '2e2e', name: "Julián", age: 25 }
+  ];
+
   const togglePersonsHandler = () => {
     setAppState({
-      persons: appState.persons,
       showPersons : !appState.showPersons
     });
   }
 
-  const changeNameHandler = (event, personId) => {
-    const modifiedPersonIndex = appState.persons.findIndex(person => { return person.id === personId; });
-
-    const persons = [
-      ...appState.persons
-    ];
-
-    const modifiedPerson = persons[modifiedPersonIndex];
-    modifiedPerson.name = event.target.value;
-
-    setAppState({
-      persons: persons,
-      showPersons: appState.showPersons
-    });
-  }
-
-  const deletePersonHandler = (personIndex) => {
-    const currentPersons = [...appState.persons];
-    currentPersons.splice(personIndex, 1);
-    
-    setAppState({
-      persons: currentPersons,
-      showPersons: appState.showPersons
-    });
-  }
-
-  let persons = null;
+  let personsToDisplay = null;
   let btnClass = null;
 
   if(appState.showPersons) {
-    persons =
-      appState.persons.map((person, index) => {
-        return(
-          //ErrorBoundary is a high order component
-          <ErrorBoundary key={person.id}>
-            <Person
-              click={() => deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              changed={(event) => changeNameHandler(event, person.id)} />
-          </ErrorBoundary>
-        );
-      }
-    );
-
+    personsToDisplay = <Persons persons={persons} />;
     btnClass = classes.red;
   }
 
-  let textClasses = [];
-  if(appState.persons.length <= 2) {
-    textClasses.push( classes.red );
-  }
-  if(appState.persons.length <= 1) {
-    textClasses.push( classes.bold );
-  }
+  // let textClasses = [];
+  // if(appState.persons.length <= 2) {
+  //   textClasses.push( classes.red );
+  // }
+  // if(appState.persons.length <= 1) {
+  //   textClasses.push( classes.bold );
+  // }
 
   return (
         <div className={classes.app}>
-          <Persons />
           <h1>Hi, I'm a React app!</h1>
-          <p className={textClasses.join(' ')}>This is a stylized text</p>
+          {/* <p className={textClasses.join(' ')}>This is a stylized text</p> */}
+
           <button
             className={btnClass}
             onClick={togglePersonsHandler}>
             Toggle persons
           </button>
-          {persons}
+          {personsToDisplay}
         </div>
 
     //We can create react apps without using JSX (behind the scenes, JSX is pure JavaScript)
