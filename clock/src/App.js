48b62b2d30152f7f8f670607bbfb3c5614310b9e
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-05-24 17:04:01 -0300

    Added eslint to Clock project and refactored components into it.

---
+++ clock/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Clock from './Clock.js'
+
+// App component
+export function App () {
+  return (<div>
+    <Clock />
+  </div>)
+}
+
+export default App;
--- clock/src/index.js
+++ clock/src/index.js
@@ -1,55 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-//App component
-function App() {
-    return(
-        <div>
-            <Clock/>
-            <Clock/>
-            <Clock/>
-        </div>
-    )
-}
-
-//Clock component
-class Clock extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date()
-        }
-    }
-
-    componentDidMount() {
-        this.timerId = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerId);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    render() {
-        return(
-            <div>
-                <h2>Hello, world!</h2>
-                <h2>It's {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        )
-    }
-}
-
-//=======================
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+  <App />,
+  document.getElementById('root')
+)
