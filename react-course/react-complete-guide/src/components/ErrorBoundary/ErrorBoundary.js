52925bee3b332503d2e1eb3dcca0ade3a2d4cbaf
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-05-25 18:06:20 -0300

    Indentation changes.

--- react-course/react-complete-guide/src/components/ErrorBoundary/ErrorBoundary.js
+++ react-course/react-complete-guide/src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 
 class ErrorBoundary extends Component {
-    state = {
-        hasError: false,
-        errorMessage: ''
-    }
+  state = {
+    hasError: false,
+    errorMessage: ''
+  }
 
-    componentDidCatch = (error, errorInfo) => {
-        this.setState({
-            hasError: true,
-            errorMessage: errorInfo
-        });
-    }
+  componentDidCatch = (error, errorInfo) => {
+    this.setState({
+      hasError: true,
+      errorMessage: errorInfo
+    });
+  }
 
-    render() {
-        if (this.state.hasError) {
-            return (
-              <div>
-                <h1>Oops! Something went wrong</h1>
-                <p>{this.state.errorMessage}</p>
-              </div>
-            );
-        } 
-        return this.props.children;
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Oops! Something went wrong</h1>
+          <p>{this.state.errorMessage}</p>
+        </div>
+      );
     }
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundary;
--- react-course/react-complete-guide/src/components/Persons/Person/Person.js
+++ react-course/react-complete-guide/src/components/Persons/Person/Person.js
@@ -9,31 +9,31 @@ import personClasses from './Person.css';
 // Fuction component WITH arrow functions
 const person = props => 
 
-    // Simulation of an error in order to test ErrorBoundaries
-    // const random = Math.random();
+  // Simulation of an error in order to test ErrorBoundaries
+  // const random = Math.random();
 
-    // if(random > 0.7) {
-    //     throw new Error('Something went wrong :(');
-    // }
+  // if(random > 0.7) {
+  //     throw new Error('Something went wrong :(');
+  // }
 
-    (
-      <div className={personClasses.person}>
-        <p onClick={props.click}>
-          Hello, I&apos;m
-          {' '}
-          {props.name}
-          {' '}
-          and I&apos;m
-          {' '}
-          {props.age}
-          {' '}
-          years old!
-        </p>
-        <p>
-          {props.children}
-        </p>
-        <input type="text" onChange={props.changed} value={props.name} />
-      </div>
-    );
+  (
+    <div className={personClasses.person}>
+      <p onClick={props.click}>
+        Hello, I&apos;m
+        {' '}
+        {props.name}
+        {' '}
+        and I&apos;m
+        {' '}
+        {props.age}
+        {' '}
+        years old!
+      </p>
+      <p>
+        {props.children}
+      </p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 
 export default person;
