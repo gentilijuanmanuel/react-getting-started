cc6ea38fb0111d0b33f178f58cf242b92d981b8e
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-06-27 07:53:51 -0300

    Small fix in reac-complete-guide project.

--- react-course/react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-course/react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -5,7 +5,7 @@ import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
   const {
-    personsLength, showPersons, clicked, login
+    personsLength, showPersons, clicked
   } = props;
   
   const authContext = useContext(AuthContext);
