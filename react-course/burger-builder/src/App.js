62876e10b15c1016a3b692e4ad1f874cac9b0e5f
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-06-27 07:41:35 -0300

    Added Layout wrapper (burger-builder project).

--- react-course/burger-builder/src/App.js
+++ react-course/burger-builder/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from '../src/components/Layout/Layout';
 
 function App() {
   return (
     <div>
-      <p>Burguer builder!</p>
+      <Layout>
+        <p>Test!</p>
+      </Layout>
     </div>
   );
 }
