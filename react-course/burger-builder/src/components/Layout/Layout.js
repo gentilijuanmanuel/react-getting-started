261cc08dbdd6afa889bde2014769d5046403d56a
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-06-27 07:53:04 -0300

    Added eslint to burger-builder project.

--- react-course/burger-builder/src/components/Layout/Layout.js
+++ react-course/burger-builder/src/components/Layout/Layout.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Aux from '../../hoc/Aux';
 
-const layout = (props) => (
-  <Aux>
-    <div>Toolbar, Sidebar, Backdrop</div>
-    <main>
-      { props.children }
-    </main>
-  </Aux>
-);
+const layout = (props) => {
+  const { children } = props;
+
+  return (
+    <Aux>
+      <div>Toolbar, Sidebar, Backdrop</div>
+      <main>
+        { children }
+      </main>
+    </Aux>
+  );
+};
+
+layout.propTypes = {
+  children: PropTypes.element.isRequired
+};
 
 export default layout;
