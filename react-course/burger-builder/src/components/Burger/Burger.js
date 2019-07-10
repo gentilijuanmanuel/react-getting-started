219fd03885dd8e5e4c02ff7d577feb015b7e9eaf
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-07-10 07:29:57 -0300

    Added explanation of some tricky lines of code in Burger.js component (burger-builder project).

--- react-course/burger-builder/src/components/Burger/Burger.js
+++ react-course/burger-builder/src/components/Burger/Burger.js
@@ -6,7 +6,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
   const { ingredients } = props;
 
-  // TODO: deeply understand this!
+  // Explanation:
+  // With Object.keys(object) we return an array with the keys of the object as elements.
+  // Then, we use the map() method to create an array for each ingredient
+  // (for example if you have two tomatoes, the array of tomatoes will have two elements),
+  // and then for each element return a BurgerIngredient with the corresponding key and type.
+  // Note:
+  // .map((_, i) => ): _ is the first argument of the callback function (current Value of the array,
+  // and we are not interested in this value) and i is the second argument of the callback,
+  // the index.
   const transformedIngredients = Object.keys(ingredients)
       .map(ingredientKey => [...Array(ingredients[ingredientKey])].map((_, i) =>
         <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />));
@@ -21,7 +29,6 @@ const burger = (props) => {
 };
 
 burger.propTypes = {
-  // TODO: see this
   ingredients: PropTypes.element.isRequired
 };
 
