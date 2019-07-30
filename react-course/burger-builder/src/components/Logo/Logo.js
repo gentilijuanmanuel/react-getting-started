35dedd69883cc56ce4dbbeee1de319ac461a0a8d
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-07-30 07:30:01 -0300

    Added navigation items in burger-builder project.

--- react-course/burger-builder/src/components/Logo/Logo.js
+++ react-course/burger-builder/src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import burgerLogo from '../../assets/burger-logo.png';
 import classes from './Logo.css';
 
-const logo = props => (
+const logo = () => (
   <div className={classes.Logo}>
     <img alt="My burger logo" src={burgerLogo} />
   </div>
---
+++ react-course/burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './NavigationItem.css';
+
+const navigationItem = (props) => {
+  const { link, active, children } = props;
+
+  return (
+    <li className={classes.NavigationItem}>
+      <a href={link} className={active ? classes.active : null}>{children}</a>
+    </li>
+  );
+};
+
+navigationItem.propTypes = {
+  link: PropTypes.string.isRequired,
+  active: PropTypes.bool,
+  children: PropTypes.string.isRequired
+};
+
+navigationItem.defaultProps = {
+  active: false
+};
+
+export default navigationItem;
