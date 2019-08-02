92d299a65108b8d33a3b6deacb7e1c26ef3ff79e
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-08-02 12:14:10 -0300

    Added SideDrawer. Made a couple of css classes mobile first (burger-builder project).

--- react-course/burger-builder/src/components/Layout/Layout.js
+++ react-course/burger-builder/src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => {
   const { children } = props;
@@ -10,6 +11,7 @@ const layout = (props) => {
   return (
     <Aux>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.Content}>
         { children }
       </main>
