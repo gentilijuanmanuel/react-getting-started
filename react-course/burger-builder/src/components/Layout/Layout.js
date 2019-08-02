9d299b83e334f8bfcfe41f495bfe5bcfd2b007f4
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-08-02 15:16:40 -0300

    Added Backdrop to SideDrawer (burger-builder project).

--- react-course/burger-builder/src/components/Layout/Layout.js
+++ react-course/burger-builder/src/components/Layout/Layout.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component } from 'react';
 import PropTypes from 'prop-types';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
-  const { children } = props;
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
 
-  return (
-    <Aux>
-      <Toolbar />
-      <SideDrawer />
-      <main className={classes.Content}>
-        { children }
-      </main>
-    </Aux>
-  );
-};
+  closeSideDrawerHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  }
 
-layout.propTypes = {
+  render() {
+    const { showSideDrawer } = this.state;
+    const { children } = this.props;
+
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer showSideDrawer={showSideDrawer} closeSideDrawer={this.closeSideDrawerHandler} />
+        <main className={classes.Content}>
+          { children }
+        </main>
+      </Aux>
+    );
+  }
+}
+
+Layout.propTypes = {
   children: PropTypes.element.isRequired
 };
 
-export default layout;
+export default Layout;
--- react-course/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ react-course/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
+  const { showSideDrawer, closeSideDrawer } = props;
+
+  let sideDrawerClasses = [classes.SideDrawer, classes.Close];
+  if (showSideDrawer) {
+    sideDrawerClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={showSideDrawer} clicked={closeSideDrawer} />
+      <div className={sideDrawerClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
+};
+
+sideDrawer.propTypes = {
+  showSideDrawer: PropTypes.bool.isRequired,
+  closeSideDrawer: PropTypes.func.isRequired
 };
 
 export default sideDrawer;
