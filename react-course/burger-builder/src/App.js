4f0c830dfef4ca8ea7fc3e8cec2d99bc77a7ffc9
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2019-09-08 15:54:29 -0300

    Small change: added BrowserRouter wrapper in index.js, removed it from App.js. Added comments related to two ways of creating the Route components.

--- react-course/burger-builder/src/App.js
+++ react-course/burger-builder/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,14 +7,15 @@ import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/" component={BurgerBuilder} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      {/* We can use the prop 'exact' in the generic path, without the Switch component */}
+      {/* <Route path="/" exact component={BurgerBuilder} />
+      <Route path="/checkout" component={Checkout} /> */}
+      <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/" component={BurgerBuilder} />
+      </Switch>
+    </Layout>
   );
 }
 
